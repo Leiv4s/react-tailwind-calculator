18b47dabe44444f303dc379adc176c32df3069aa
leiv4s <leivashenrique12@gmail.com>
2023-12-24 04:15:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Container from "./components/containerCalculadora/container.jsx";
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+        <Container />
+    </>
+  )
+}
+
+export default App
